ef17aa87bc8eba93a7e9f063241fa215ce21a9f0
chiara.ruggi <chiara.ruggi1998@gmail.com>
2023-11-13 09:47:31 +0100

    cycle for print on console

--- js/main.js
+++ js/main.js
@@ -32,10 +32,9 @@ const teamMember = [
 
 ];
 
+for (let i = 0; i < teamMember.length; i++) {
 
-Wayne Barnett	Founder & CEO	        wayne-barnett-founder-ceo.jpg
-Angela Caroll	Chief Editor	        angela-caroll-chief-editor.jpg
-Walter Gordon	Office Manager	        walter-gordon-office-manager.jpg
-Angela Lopez	Social Media Manager	angela-lopez-social-media-manager.jpg
-Scott Estrada	Developer	            scott-estrada-developer.jpg
-Barbara Ramos	Graphic Designer	    barbara-ramos-graphic-designer.jpg
+    for(let chiave in teamMember[i]) {
+        console.log(`Nome: ${teamMember[i].name} - Cognome: ${teamMember[i].surname} - Eta: ${teamMember[i].role}`);
+    }
+}
