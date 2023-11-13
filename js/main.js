c2ea39fb33fc0bdd8c4ea45074a7e6699719298e
chiara.ruggi <chiara.ruggi1998@gmail.com>
2023-11-13 09:44:32 +0100

    create object

---
+++ js/main.js
@@ -0,0 +1,41 @@
+const teamMember = [
+    {
+        name: "Wayne",
+        surname: "Barnett",
+        role: "Founder & CEO",
+    },
+    {
+        name: "Angela",
+        surname: "Caroll",
+        role: "Chief Editor",
+    },
+    {
+        name: "Walter",
+        surname: "Gordon",
+        role: "Office Manager",
+    },
+    {
+        name: "Angela",
+        surname: "Lopez",
+        role: "Social Media Manager",
+    },
+    {
+        name: "Scott",
+        surname: "Estrada",
+        role: "Developer",
+    },
+    {
+        name: "Barbara",
+        surname: "Ramos",
+        role: "Graphic Designer",
+    },
+
+];
+
+
+Wayne Barnett	Founder & CEO	        wayne-barnett-founder-ceo.jpg
+Angela Caroll	Chief Editor	        angela-caroll-chief-editor.jpg
+Walter Gordon	Office Manager	        walter-gordon-office-manager.jpg
+Angela Lopez	Social Media Manager	angela-lopez-social-media-manager.jpg
+Scott Estrada	Developer	            scott-estrada-developer.jpg
+Barbara Ramos	Graphic Designer	    barbara-ramos-graphic-designer.jpg
